20f48229dad8b5fb1a1a0f74c5cd5a268819a316
cauanrricardo <cauanrricardo@gmail.com>
2026-01-08 15:14:43 -0300

    feat: adicao de pasta test/service + classe de teste do Motorista usando mock

--- src/main/java/com/parking/api/repository/MotoristaRepository.java
+++ src/main/java/com/parking/api/repository/MotoristaRepository.java
@@ -3,5 +3,8 @@ package com.parking.api.repository;
 import com.parking.api.model.Motorista;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MotoristaRepository extends JpaRepository<Motorista, Long> {
+    Optional<Motorista> findByRg(String rg);
 }
---
+++ src/test/java/com/parking/api/service/MotoristaServiceTest.java
@@ -0,0 +1,49 @@
+package com.parking.api.service;
+
+import com.parking.api.model.Motorista;
+import com.parking.api.repository.MotoristaRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class MotoristaServiceTest {
+
+    @Mock //cria o duble do Banco
+    private MotoristaRepository repository;
+
+    @InjectMocks //injeta o duble no service
+    private MotoristaService service;
+
+    @Test
+    void naoDeveCadastrarMotoristaComRgDuplicado() {
+        //given
+        String rgDuplicado = "123";
+
+        Motorista motoristaNovo = new Motorista();
+        motoristaNovo.setRg(rgDuplicado);
+        motoristaNovo.setNomeCompleto("Cauan Mock Teste");
+
+        Motorista motoristaAntigo = new Motorista();
+        motoristaAntigo.setId(1L);
+        motoristaAntigo.setRg(rgDuplicado);
+
+        //mock simula aq um rg ja cadastrado
+        when(repository.findByRg(rgDuplicado)).thenReturn(Optional.of(motoristaAntigo));
+
+        //execucao e validacao (when / then)
+        //o teste espera lancar uma execao ao tentar falhar
+        assertThrows(ResponseStatusException .class, () -> {
+            service.salvar(motoristaNovo);
+        });
+    }
+
+}
