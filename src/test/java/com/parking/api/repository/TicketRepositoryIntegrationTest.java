d206914903df94be3bdaa7043a1fd78472166acd
cauanrricardo <cauanrricardo@gmail.com>
2026-03-18 12:40:29 -0300

    feat: adicao de mais testes de integracao tickets

--- src/test/java/com/parking/api/repository/TicketRepositoryIntegrationTest.java
+++ src/test/java/com/parking/api/repository/TicketRepositoryIntegrationTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -41,6 +42,50 @@ public class TicketRepositoryIntegrationTest {
 
         assertTrue(resultado.isPresent());
         assertEquals(ticketSalvo.getId(), resultado.get().getId());
+    }
+
+    @Test
+    void deveRetornarRegistroInexistente(){
+        Optional<Ticket> resultadoInexistente = repository.findById(99L);
+
+        assertFalse(resultadoInexistente.isPresent());
+
+    }
+
+    @Test
+    void deveRetornarListaCorretaDetickets(){
+    Ticket t1 = new Ticket();
+    Ticket t2 = new Ticket();
+
+    t1.setDataEntrada(LocalDateTime.now().minusMinutes(30));
+    t2.setDataEntrada(LocalDateTime.now().minusMinutes(20));
+
+    repository.save(t1);
+    repository.save(t2);
+
+    List<Ticket> tickets = repository.findAll();
+
+    assertEquals(2, tickets.size());
+
+    }
+
+    @Test
+    void deveRetornarTrueSeExisteId(){
+        Ticket ticket = new Ticket();
+        ticket.setDataEntrada(LocalDateTime.now().minusMinutes(30));
+
+        Ticket salvo = repository.save(ticket);
+
+        Optional<Ticket> idExistente = repository.findById(salvo.getId());
+
+        assertTrue(idExistente.isPresent());
+        assertEquals(salvo.getId(), idExistente.get().getId());
+    }
+
+    @Test @DisplayName("Deve retornar vazio ao buscar id inexistente")
+    void deveRetornarVazioQuandoIdNaoExiste() {
+        Optional<Ticket> encontrado = repository.findById(99L);
+        assertTrue(encontrado.isEmpty());
     }
 
 }
